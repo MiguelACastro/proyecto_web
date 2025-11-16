87bc555169f856f2a14ca86baa7d406f61016863
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-11-15 20:39:39 -0800

    Separar footer y header

---
+++ public/index.php
@@ -0,0 +1,124 @@
+<?php
+    include __DIR__.'/../src/views/layouts/header.php';
+?> 
+    <main>
+        <section class="hero">
+            <div class="hero-content">
+                <h1 class="hero-title">ThinkPad X1</h1>
+                <p class="hero-text">
+                    La serie Lenovo ThinkPad X1 ofrece laptops de alta gama para profesionales, combinando diseño
+                    elegante, rendimiento
+                    excepcional y versatilidad, desde ultraligeras hasta 2 en 1 y plegables, con fiabilidad, seguridad y
+                    durabilidad
+                    garantizadas.
+                </p>
+                <button class="hero-button">Ver producto</button>
+            </div>
+        </section>
+
+        <h1 class="section-title">Ofertas del día</h1>
+        <section class="container">
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+        </section>
+
+        <img src="<?=RESOURCES_PATH?>/imagen 2.png" class="content-image">
+
+        <h1 class="section-title">Mas vendidos</h1>
+        <section class="container">
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+
+            <div class="card">
+                <img class="card-image" src="<?=RESOURCES_PATH?>/nintendo-switch-2-.jpg" alt="">
+                <h2 class="card-title">Nintendo Switch 2</h2>
+                <p class="card-text">Ha llegado la siguiente evolución de la consola Nintendo Switch!</p>
+                <button class="card-button" onclick="location.href='../src/views/producto.php'">Leer Más</button>
+            </div>
+        </section>
+
+        <section class="newsletter">
+            <div>
+                <h3 class="newsletter-title">Suscribete al newsletter</h3>
+                <p class="newsletter-text">Registra tu correo para recibir noticias y promociones</p>
+            </div>
+            <form action="post">
+                <input type="email" placeholder="Ingresa tu correo" id="email" class="email-input">
+                <button type="submit" class="submit-button">Enviar</button>
+            </form>
+        </section>
+     </main>
+<?php
+    include __DIR__.'/../src/views/layouts/footer.php';
+?>
---
+++ src/helpers/functions.php
@@ -0,0 +1,7 @@
+<?php 
+
+$config = require __DIR__.'/../config/config.php';
+
+define('BASE_PATH', $config['base_url']);
+define('RESOURCES_PATH', $config['resources_url']);
+define('SRC_PATH', $config['src_url']);
