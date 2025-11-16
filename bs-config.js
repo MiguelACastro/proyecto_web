3b63afe5c5482875bc2e14368b947bf3267b8a2a
Miguel Castro <mcastro_23@alu.uabcs.mx>
2025-11-15 18:00:56 -0800

    Añadir BrowserSync

---
+++ bs-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    proxy: "127.0.0.1:80",        
+    startPath: "/proyecto/public",
+    files: [
+        "./public/*.css",      
+        "./public/js/*.js",
+        "./public/*.php",
+        
+        "./src/**/*.php",
+        "./src/**/*.css",
+        "./src/**/*.js"
+    ],
+
+    ignore: [
+        "./public/output.css"
+    ],
+    reloadDelay: 300,
+    notify: false,
+    open: true
+};
